1135abd8596b665ad4adacb6e159020c9165ff37
Leo <leoli_082000@hotmail.com>
2026-07-25 01:45:26 +0800

    Export public service API

---
+++ src/index.mjs
@@ -0,0 +1,3 @@
+export { inspectProject, reviewProject } from './service.mjs';
+export { loadManifest, normalizeManifest } from './core/manifest.mjs';
+export { summarizeReceipt } from './core/receipt.mjs';
